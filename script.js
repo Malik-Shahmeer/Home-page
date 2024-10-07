b6dbd13aca2b0811a88fa28e284f8f4b14c8db21
Muhammad Shahmeer <138344525+Malik-Shahmeer@users.noreply.github.com>
2024-10-07 06:21:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+// Swiper Slider
+// window.addEventListener('resize', () => {
+//     const containerWidth = document.querySelector('.slider-container').offsetWidth;
+
+//     if (containerWidth >= 900) {
+//         imagesPerView = 3;
+//     } else if (containerWidth >= 600) {
+//         imagesPerView = 2;
+//     } else {
+//         imagesPerView = 1;
+//     }
+
+    // Adjust the size of each image to match the number of visible images
+    // sliderImages.forEach(image => {
+    //     image.style.flex = `1 0 ${100 / imagesPerView}%`;
+    //     image.style.maxWidth = `${100 / imagesPerView}%`;
+    // });
+
+    // Recalculate position based on the updated images per view
+//     showSlide(currentIndex);
+// });
+
+// Initialize the slider based on screen size
+// window.dispatchEvent(new Event('resize'));
+
+
+
+
+// // Navigation
+// function showSlidebar() {
+//   const sidebar = document.querySelector('.sidebar')
+//   sidebar.style.display = 'flex'
+// }
+
+// section-seven
+// var swiper = new Running(".myQuality", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// navigation
+
+
+// window.dispatchEvent(new Event('resize')); // Initialize the layout
+// const navbar = document.getElementById('navbar');
+// const toggleButton = document.querySelector('.toggle-button');
+// const navLinks = document.querySelector('.nav-links');
+// const navBrand = document.querySelector('.nav-brand');
+// const sidebar = document.createElement('div');
+// sidebar.className = 'sidebar';
+// sidebar.innerHTML = navLinks.outerHTML;
+// document.body.appendChild(sidebar);
+
+// toggleButton.addEventListener('click', () => {
+//     sidebar.classList.toggle('active');
+// });
+
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth >= 768) {
+//         sidebar.classList.remove('active');
+//         navLinks.style.display = 'flex';
+//         navBrand.classList.remove('center-logo');
+//         toggleButton.style.display = 'none';
+//     } else {
+//         navLinks.style.display = 'none';
+//         navBrand.classList.add('center-logo');
+//         toggleButton.style.display = 'block';
+//     }
+// });
+
+// window.dispatchEvent(new Event('resize')); // Initialize the layout
+
+
+
+
